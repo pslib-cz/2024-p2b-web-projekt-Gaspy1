19edba8fb710d585b47af1f4a93fd9c19d4d5cd6
Adam Gašparín <adamgasparin@gmail.com>
2025-05-14 21:08:01 +0200

    +skorohotovo

--- src/scripts/email.js
+++ src/scripts/email.js
@@ -1,19 +1,9 @@
 import emailjs from 'emailjs-com';
 
-document.getElementById('buyButton').addEventListener('click', function () {
-  const templateParams = {
-    name: 'Uživatel', // Můžete nahradit dynamickými daty
-    message: 'Někdo chce nakoupit produkty z vašeho webu.',
+document.getElementById('buyButton').addEventListener('click', e => {
+  const form = {
+    email: document.getElementById('email').value
   };
 
-  emailjs
-    .send('service_xdhyitf', 'template_9r6ei2f', templateParams, '-oiaFJC2eOtlPZ449')
-    .then(
-      function (response) {
-        alert('E-mail byl úspěšně odeslán!');
-      },
-      function (error) {
-        alert('Došlo k chybě při odesílání e-mailu: ' + error.text);
-      }
-    );
+  emailjs.send('service_xdhyitf', 'template_9r6ei2f', form)
 });
