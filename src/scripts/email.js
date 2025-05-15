b883c424644c11f8e3a20ab3e9aabfea1b4cf9ef
Adam Gašparín <adamgasparin@gmail.com>
2025-05-15 15:40:05 +0200

    +myslimzehotovo

--- src/scripts/email.js
+++ src/scripts/email.js
@@ -3,9 +3,11 @@ import emailjs from 'emailjs-com';
 emailjs.init('-oiaFJC2eOtlPZ449');
 document.getElementById('buyButton').addEventListener('click', e => {
   const form = {
-    email: document.getElementById('email').value
+    email: document.getElementById('email').value,
+    jmeno: document.getElementById('jmeno').value,
   };
   emailjs.send('service_xdhyitf', 'template_9r6ei2f', {
-    email: form.email
+    email: form.email,
+    jmeno: form.jmeno,
   })
 });
